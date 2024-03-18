d52a0c35cf31f00b0679a3213b8f5deea92db1e6
Andrew Bello <92801912+Andrew-Bello@users.noreply.github.com>
2024-03-18 12:24:14 +0000

    Modal redone!

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,18 +4,16 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
-const closeModal = function () {
+function closeModal() {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-};
+}
 
-const openModal = function () {
-  console.log('Button Clicked');
+function openModal() {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
-};
+}
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', openModal);
@@ -24,7 +22,9 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', function (e) {
+document.addEventListener('keydown', e => {
+  console.log(e.key);
+
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
