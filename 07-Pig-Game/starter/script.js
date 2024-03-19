2ccd9404db49e23a67e86ce3691ac6a318296340
Andrew Bello <92801912+Andrew-Bello@users.noreply.github.com>
2024-03-19 20:45:47 +0000

    Revisited pig game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -4,77 +4,67 @@
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('score--1');
-const current0El = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
+const score1El = document.querySelector('#score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-let scores, currentScore, activePlayer, playing;
+// Starting conditions
+let currentScore, scores, activePlayer, playing;
 
-//Starting Conditions
 function init() {
-  scores = [0, 0];
   currentScore = 0;
   activePlayer = 0;
+  scores = [0, 0];
   playing = true;
 
+  diceEl.classList.add('hidden');
   score0El.textContent = 0;
   score1El.textContent = 0;
   current0El.textContent = 0;
   current1El.textContent = 0;
-
-  diceEl.classList.add('hidden');
   player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner', 'player--active');
+  player1El.classList.remove('player--winner');
   player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
 }
 
 init();
 
-const switchPlayer = function () {
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
-  activePlayer = activePlayer === 0 ? 1 : 0;
-  currentScore = 0;
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
-};
-
 //Rolling dice functionality
-btnRoll.addEventListener('click', function () {
+
+btnRoll.addEventListener('click', () => {
   if (playing) {
-    //  1. Generating a random dice roll
+    //1. Generate a random dice roll
     const dice = Math.floor(Math.random() * 6) + 1;
-    //  2. Display dice
+    //2. Display dice
     diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
-    //  3. Check for rolled 1: true, switch to next player
+    //3. Check for roller 1: if true
     if (dice !== 1) {
-      // Add dice to the current score
       currentScore += dice;
-
-      document.getElementById(`current--${activePlayer}`).textContent =
+      document.querySelector(`#current--${activePlayer}`).textContent =
         currentScore;
     } else {
-      //Switch to next player
+      //switch to next player
       switchPlayer();
     }
   }
 });
 
-btnHold.addEventListener('click', function () {
+btnHold.addEventListener('click', () => {
   if (playing) {
-    // 1. Add current score to active player's score
+    // 1. Add current score to active plater's score
     scores[activePlayer] += currentScore;
-
-    document.getElementById(`score--${activePlayer}`).textContent =
+    document.querySelector(`#score--${activePlayer}`).textContent =
       scores[activePlayer];
-    // 2. Check if player's score >= 100
+    // 2. Check if layer's score is >= 100
+
     if (scores[activePlayer] >= 100) {
       // Finish the game
-      diceEl.classList.add('hidden');
       playing = false;
       document
         .querySelector(`.player--${activePlayer}`)
@@ -82,6 +72,7 @@ btnHold.addEventListener('click', function () {
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.remove('player--active');
+      diceEl.classList.add('hidden');
     } else {
       switchPlayer();
     }
@@ -89,5 +80,13 @@ btnHold.addEventListener('click', function () {
 
   // Switch to the next player
 });
+
+function switchPlayer() {
+  document.querySelector(`#current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+}
 
 btnNew.addEventListener('click', init);
